615526f483bec54122e181b5efa14134dfbfc44e
阿勇 <1554741635@qq.com>
2024-08-07 17:04:48 +0800

    Hello

--- src/main/java/com/atguigu/HelloGit.java
+++ src/main/java/com/atguigu/HelloGit.java
@@ -6,8 +6,7 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Hello GitHub");
-        System.out.println("Hello GitHub");
-        System.out.println("Hello GitHub");
+
         System.out.println("Hello GitHub");
     }
 
